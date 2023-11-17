914502af7b080f09e5cd847a08adcdf3ac5e9bc4
tonikuikka <toni.kuikka@hotmail.com>
2023-11-17 20:01:35 +0200

    Initialize the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,24 +3,17 @@ export default defineNuxtConfig({
   ssr: false,
   app: {
     head: {
-      title: 'Nuxt3 x GitHub Pages',
+      title: '',
       meta: [
         { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: 'Dario Tecchia\' personal website!' },
-        { name: 'format-detection', content: 'telephone=no' }
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
       ],
       link: [
         { rel: 'icon', type: 'image/svg', href: '/favicon.svg' }
       ]
-    },
-    baseURL: '/nuxt3-ghpages/'
+    }
   },
   modules: [
   ],
-  css: [
-    "bootstrap/dist/css/bootstrap.min.css",
-    "bootstrap-vue-3/dist/bootstrap-vue-3.css",
-    '@/assets/style/main.scss',
-  ]
+  css: ['~/assets/style/main.css']
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>
-    <h1>Nuxt3 x GitHup Pages</h1>
-  </div>
+    <div id="page-wrapper">
+        <header>
+            <h1>Toni Kuikka</h1>
+        </header>
+        <div id="page-content">
+            <p>testttttttttttttttttttt</p>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+    header {
+        padding: 30px;
+    }
+    h1 {
+        color: var(--headers);
+        font-family: 'Courier New', Courier, monospace;
+    }
+    #page-content {
+        color: var(--paragraphs);
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    #page-content > p {
+        animation: typing 10s steps(100) forwards;
+        width: 0;
+        overflow: hidden;
+        border-right: 2px var(--headers);
+    }
+    @keyframes typing {
+        from { width: 0 }
+        to { width: 100%; }
+    }
+</style>
